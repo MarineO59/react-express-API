bd9c25c8a3c416c756632d4535866ce5c04b655a
TheDiabolicalKid <marine.opsommer@gmail.com>
2026-02-23 14:36:44 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+const sampleEmployees = {
+    name: {
+        first: "Charlie",
+        last: "Thompson",
+    },
+    email: "charlie.thompson@example.com",
+    picture: {
+        medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+    },
+};
+
+
+app.get("/api/employees", (req, res) => {
+    res.json({ results: [sampleEmployees] });
+});
+
+
+
+const port = 3310;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
